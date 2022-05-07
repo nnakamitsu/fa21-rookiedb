7646fd4d5d11025d69ce0228b38960593411d72f
caowei <weicao92@163.com>
2022-05-07 23:36:08 +0800

    proj0: setup

--- src/main/java/edu/berkeley/cs186/database/databox/StringDataBox.java
+++ src/main/java/edu/berkeley/cs186/database/databox/StringDataBox.java
@@ -56,7 +56,7 @@ public class StringDataBox extends DataBox {
     @Override
     public String toString() {
         // TODO(proj0): replace the following line with `return s;`
-        return "FIX ME";
+        return s;
     }
 
     @Override
